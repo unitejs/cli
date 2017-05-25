2ca698d6e780d928572a2dd734af981dba73f9cc
Martyn Janes <martyn@unitejs.com>
2017-05-25 14:35:22 +0300

    Added Engine Questions

--- src/unitejs-cli.ts
+++ src/unitejs-cli.ts
@@ -1,7 +1,7 @@
 /**
  * Main entry point.
  */
-import { Engine, IDisplay, IEngine, ILogger } from "unitejs-core/dist/unitejs-core";
+import { Engine, IDisplay, IEngine, IKeyValue, ILogger, UniteLanguage } from "unitejs-core/dist/unitejs-core";
 import { CommandLineArgConstants } from "./commandLineArgConstants";
 import { CommandLineCommandConstants } from "./commandLineCommandConstants";
 import { CommandLineParser } from "./commandLineParser";
@@ -9,6 +9,9 @@ import { Display } from "./display";
 import { Logger } from "./logger";
 
 export class CLI {
+    private static APP_NAME: string = "UniteJS";
+    private static DEFAULT_LOG: string = "unite.log";
+
     public run(process: NodeJS.Process): number {
         let logger: ILogger | undefined;
         let ret: number;
@@ -19,7 +22,7 @@ export class CLI {
 
             logger = new Logger(commandLineParser.getNumberArgument(CommandLineArgConstants.LOG_LEVEL),
                                 commandLineParser.getStringArgument(CommandLineArgConstants.LOG_FILE),
-                                "unite.log");
+                                CLI.DEFAULT_LOG);
             logger.info("Session Started");
 
             const display: IDisplay = new Display(process, commandLineParser.hasArgument(CommandLineArgConstants.NO_COLOR));
@@ -63,7 +66,10 @@ export class CLI {
 
             case CommandLineCommandConstants.INIT: {
                 const engine: IEngine = new Engine(logger, display);
-                engine.init(args);
+
+                let name = this.ask("What is the name of your package");
+                let language = this.askEnumeration<UniteLanguage>("Which development language do you want to use", engine.getAvailableLanguages());
+                engine.init(name, language);
                 break;
             }
 
@@ -84,10 +90,10 @@ export class CLI {
     private displayBanner(logger: ILogger, display: IDisplay, includeTitle: boolean): void {
         const packageJson = require("../package.json");
         if (includeTitle) {
-            display.banner("UniteJS CLI");
+            display.banner(CLI.APP_NAME + " CLI");
         }
         display.banner(packageJson && packageJson.version ? "v" + packageJson.version : " Unknown Version");
-        logger.info("UniteJS Version", { version: packageJson.version } );
+        logger.info(CLI.APP_NAME, { version: packageJson.version } );
         if (includeTitle) {
             display.banner("");
         }
@@ -97,5 +103,13 @@ export class CLI {
         display.info("This is the help");
 
         return 0;
+    }
+
+    private ask(question: string): string {
+        return "";
+    }
+
+    private askEnumeration<T>(question: string, values: IKeyValue<T>[]): T {
+        return values[0];
     }
 }
